e2428249b54d3df9360a7ed9de3eb509633106d1
MalkavianxX <mot46000@gmail.com>
2023-12-03 01:50:23 -0600

    REQ

--- valida/views.py
+++ valida/views.py
@@ -6,12 +6,11 @@ def view_start(request):
 
 def get_licencia(request,XWOPSLT,FFTWRPTO):
     #get_licencia/3/SHHHA
-    url = 'http://localhost:8000/licenciasvalidar_licencia/' + str(XWOPSLT) +'/'+str("CUALQUIERCOSA") # La URL de tu API
+    url = 'https://licenapp.cloud//licenciasvalidar_licencia/' + str(XWOPSLT) +'/'+str("CUALQUIERCOSA") # La URL de tu API
     response = requests.get(url)  # Hacer la petición GET
     if response.status_code == 200:
         data = response.json()  # Convertir la respuesta a JSON
         print(data)
         return render(request, 'valida/ok.html',{'data':data})
-
     else:
         return render(request, 'valida/no.html')
